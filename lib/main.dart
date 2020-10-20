b4785238f5113e487339b1b54e555674a24fd22c
masafumi kato <xslash1105@gmail.com>
2020-10-21 02:42:50 +0900

    [add] init commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              'I am Rich',
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.black,
+      ),
+    ),
+  );
+}
